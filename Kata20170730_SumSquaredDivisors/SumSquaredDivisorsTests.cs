28f3217d9f38c7780476fbf388ec275dda3e49b0
cash <cash@cashwu.com>
2017-07-30 12:11:43 +0800

    m 1, n 1 test case

--- Kata20170730_SumSquaredDivisors/SumSquaredDivisorsTests.cs
+++ Kata20170730_SumSquaredDivisors/SumSquaredDivisorsTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170730_SumSquaredDivisors
     public class SumSquaredDivisorsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void m_1_n_1_should_return_1_1()
         {
+            AssertSumSquaredDivisorsShouldBe(1, 1, "[[1, 1]]");
+        }
+
+        private static void AssertSumSquaredDivisorsShouldBe(long m, long n, string expected)
+        {
+            var sumSquaredDivisors = new SumSquaredDivisors();
+            var actual = sumSquaredDivisors.listSquared(m, n);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class SumSquaredDivisors
+    {
+        public string listSquared(long m, long n)
+        {
+            return $"[[{m}, {n}]]";
         }
     }
 }
