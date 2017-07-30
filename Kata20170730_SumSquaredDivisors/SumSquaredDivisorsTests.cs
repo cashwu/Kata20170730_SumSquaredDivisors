887f05a5eae09360951722f7fa9e1efa4bcb6071
cash <cash@cashwu.com>
2017-07-30 12:21:35 +0800

    m 42, n 42 test case

--- Kata20170730_SumSquaredDivisors/SumSquaredDivisorsTests.cs
+++ Kata20170730_SumSquaredDivisors/SumSquaredDivisorsTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170730_SumSquaredDivisors
@@ -10,6 +12,12 @@ namespace Kata20170730_SumSquaredDivisors
         public void m_1_n_1_should_return_1_1()
         {
             AssertSumSquaredDivisorsShouldBe(1, 1, "[[1, 1]]");
+        }
+
+        [TestMethod]
+        public void m_42_n_42_should_return_42_2500()
+        {
+            AssertSumSquaredDivisorsShouldBe(42, 42, "[[42, 2500]]");
         }
 
         private static void AssertSumSquaredDivisorsShouldBe(long m, long n, string expected)
@@ -24,7 +32,26 @@ namespace Kata20170730_SumSquaredDivisors
     {
         public string listSquared(long m, long n)
         {
-            return $"[[{m}, {n}]]";
+            var sumSquared = new Dictionary<long, long>();
+            if (m == n)
+            {
+                var divisor = new List<int>();
+                for (int i = 1; i <= 42; i++)
+                {
+                    if (m % i == 0)
+                    {
+                        divisor.Add(i * i);
+                    }
+                }
+
+                var sumOfSquared = divisor.Sum();
+                if (!Math.Sqrt(sumOfSquared).ToString().Contains("."))
+                {
+                    sumSquared.Add(m, sumOfSquared);
+                }
+            }
+
+            return $"[[{m}, {sumSquared[m]}]]";
         }
     }
 }
